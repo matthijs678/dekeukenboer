8db39a74f06808a50a9b38697b1034b4ff5f3c08
Dennis van Riet <dvriet@ictmbo.nl>
2021-06-22 11:39:05 +0200

    initial upload

--- src/handlemessage.php
+++ src/handlemessage.php
@@ -3,19 +3,22 @@
 // Check if name, email and message are given
 if ( ! empty($_POST['name']) && ! empty($_POST['email']) && ! empty($_POST['message'])) {
 
-    // TODO: Check if e-mail is valid (now always true) by using laminas-validator package: https://packagist.org/packages/laminas/laminas-validator
-    if (true) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-        // When true, add message to table messages (you can find the sql in keukenboer.sql)
-        $sql = "INSERT INTO message(name, email, message) VALUES (?, ?, ?)";
-        $mysqli = new mysqli('localhost', 'root', 'root', 'keukenboer', 8888);
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("sss", $_POST['name'],$_POST['email'],$_POST['message']);
-        if ($stmt->execute()){
-            echo "Dank voor uw bericht";
-        }
+    // TODO: Check if e-mail is valid by using laminas-validator package: https://packagist.org/packages/laminas/laminas-validator
+
+    // When email is valid, add the message to table messages (you can find the sql in keukenboer.sql)
+    $sql = "INSERT INTO message(name, email, message) VALUES (?, ?, ?)";
+    $mysqli = new mysqli('localhost', 'root', 'root', 'keukenboer', 8888);
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("sss", $name, $email, $message);
+    if ($stmt->execute()){
+        echo "Dank voor uw bericht";
     }
+
 }
 
-header("Refresh:2; url=index.php");
+header("Refresh:6; url=index.php");
 
